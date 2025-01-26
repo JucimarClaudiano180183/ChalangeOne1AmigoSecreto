c13571464d7cbef124700e21055bccf1db87bf6d
Jucimar Claudiano <jucimarcoelho@hotmail.com>
2025-01-25 21:16:21 -0300

    Implementando a função pegar nome e inserir na lista

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// criando a variavel do tipo lista  para armazenar o nome
+
+let amigos = [];
+
+function pegarValor(){
+    let amigo = document.getElementById('amigo').value;
+    return amigo;
+}
+
+//atribuir ação para o botão adcionar
+
+function adicionarAmigo() {
+
+    amigos.push(pegarValor());
+    console.log(amigos);
+
+}
+
+adicionarAmigo();
+
+
+
+
+
+
